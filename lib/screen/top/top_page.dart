b1afb50ae76e4951daa1da80b4937c1eb78b4ace
seigibus <seigibus@icloud.com>
2020-09-26 20:10:02 +0900

    #5 見た目を作成

--- lib/screen/top/top_page.dart
+++ lib/screen/top/top_page.dart
@@ -6,16 +6,25 @@ class TopPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader read) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('トップページ'),
-        ),
-        body: Container(
-          child: RaisedButton(
-            onPressed: () async {
-              await context.read(authProvider).logout();
-            },
-            child: const Text('ログアウト'),
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text('トップページ'),
+      ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          _bottomBarItem(const Icon(Icons.calendar_today), 'カレンダー'),
+          _bottomBarItem(const Icon(Icons.person), 'マイページ')
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
+}
+
+BottomNavigationBarItem _bottomBarItem(Widget icon, String title) {
+  return BottomNavigationBarItem(icon: icon, title: Text(title));
 }
