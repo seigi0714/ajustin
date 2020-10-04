45125d8b18c3758fc57d795adc74fd50c630e5a1
seigibus <seigibus@icloud.com>
2020-10-04 14:08:39 +0900

    ユーザー情報をfirestoreを参照して保持する

--- lib/domain/user_model.dart
+++ lib/domain/user_model.dart
@@ -1,5 +1,13 @@
-class User {
-  User._(this.userID, this.name, this.iconURL, this.created);
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserData {
+  UserData._(this.userID, this.name, this.iconURL, this.created);
+
+  factory UserData.fromFireStore(DocumentSnapshot doc) {
+    return UserData._(doc.id, doc.data()['name'] ?? '',
+        doc.data()['imageURL'] ?? '', doc.data()['createdAt']);
+  }
+
   final String userID;
   final String name;
   final String iconURL;
